81ab5969deee9f3429ee7fd5718d5193cf8cc0b5
Zach <footezo@rose-hulman.edu>
2019-01-31 12:33:41 -0500

    Added needed service requests, just need to make the services

--- Frontend/compile-io-frontend-app/src/app/change-homework/change-homework.component.ts
+++ Frontend/compile-io-frontend-app/src/app/change-homework/change-homework.component.ts
@@ -9,12 +9,30 @@ export class ChangeHomeworkComponent implements OnInit {
 
   @Input() givenClass: string;
   @Input() homework: string;
-
+  name: string;
+  timeout: string;
+  visible: boolean;
+  language: string;
+  file: File;
+  homeworkInfo: FormData;
   constructor() { }
 
+  getHomework() {
+    this.courseService.getHomeworkInfo(this.givenClass, this.homework).subscribe({
+      next: x => this.homeworkInfo = x,
+      error: err => console.log("GET CLASSES ERROR: " + err),
+      complete: () => console.log("got classes")
+    });
+    this.name = this.homeworkInfo.get("name").toString();
+    this.timeout = this.homeworkInfo.get("timeout").toString();
+    //this.visible = this.homeworkInfo.get("visible");
+    this.language = this.homeworkInfo.get("language").toString();
+    //this.file = this.homeworkInfo.get("file");
+  }
 
-  
+
   ngOnInit() {
+    this.getHomework();
   }
 
 }
