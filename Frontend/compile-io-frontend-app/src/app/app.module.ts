f96ced0d532f530951da0c79e5227e10425bf009
Zach <footezo@rose-hulman.edu>
2019-02-06 15:28:41 -0500

    refactored adding and changing to not rewrite code

--- Frontend/compile-io-frontend-app/src/app/app.module.ts
+++ Frontend/compile-io-frontend-app/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { ROUTING } from './app-routing/app-routing.module';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthenticationService } from './services/authentication.service';
-import { AddHomeworkComponent } from './add-homework/add-homework.component';
 import { ChangeHomeworkComponent } from './change-homework/change-homework.component';
 
 @NgModule({
@@ -20,7 +19,6 @@ import { ChangeHomeworkComponent } from './change-homework/change-homework.compo
     LoginComponent,
     UserPageComponent,
     HomeworkPageComponent,
-    AddHomeworkComponent,
     ChangeHomeworkComponent
   ],
   imports: [
--- Frontend/compile-io-frontend-app/src/app/change-homework/change-homework.component.ts
+++ Frontend/compile-io-frontend-app/src/app/change-homework/change-homework.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CourseService } from '../services/course.service';
+import { TestService } from '../services/test.service';
 
 @Component({
   selector: 'app-change-homework',
@@ -7,33 +8,79 @@ import { CourseService } from '../services/course.service';
   styleUrls: ['./change-homework.component.css']
 })
 export class ChangeHomeworkComponent implements OnInit {
-
-  @Input() givenClass: string;
-  @Input() homework: string;
+  @Input() userName: string;
+  @Input() className: string;
+  @Input() hwkName: string;
+  @Input() newHwk: boolean;
   name: string;
   timeout: string;
-  visible: boolean;
   language: string;
   file: File;
-  homeworkInfo: FormData;
-  constructor(private courseService: CourseService) {}
-
-  getHomework() {
-    this.courseService.getHomeworkInfo(this.givenClass, this.homework).subscribe({
-      next: x => this.homeworkInfo = x,
-      error: err => console.log("GET CLASSES ERROR: " + err),
-      complete: () => console.log("got classes")
-    });
-    this.name = this.homeworkInfo.get("name").toString();
-    this.timeout = this.homeworkInfo.get("timeout").toString();
-    //this.visible = this.homeworkInfo.get("visible");
-    this.language = this.homeworkInfo.get("language").toString();
-    //this.file = this.homeworkInfo.get("file");
+  hwkInfo: FormData;
+  constructor(private courseService: CourseService, private testService: TestService) {
+    
   }
 
+  getHomework() {
+    var stuff = [];
+    this.courseService.getHomeworkInfo(this.className, this.hwkName).subscribe({
+      next: x => this.hwkInfo = x,
+      error: err => console.log("GET HWK INFO ERROR: " + err),
+      complete: () => console.log("got Hwk Info")
+    });
+    this.name = this.hwkInfo.get("name").toString();
+    this.timeout = this.hwkInfo.get("timeout").toString();
+    //this.visible = this.hwkInfo.get("visible");
+    this.language = this.hwkInfo.get("language").toString();
+    //this.file = this.hwkInfo.get("file");
+  }
+  submit(name: string,
+        timeout: number,
+        visible: boolean,
+        language: string,
+        size: number,
+        tries: number,
+        startDate: Date,
+        endDate: Date) {
+
+    console.log("THIS IS THE FILE FROM SUBMIT: " + this.file);
+    this.hwkInfo = new FormData();
+    this.hwkInfo.append("name", name);
+    this.hwkInfo.append("timeout", timeout.toString());
+    this.hwkInfo.append("language", language);
+    this.hwkInfo.append("size", size.toString());
+    this.hwkInfo.append("tries", tries.toString());
+    if (this.newHwk) {
+      this.testService.addHwk(this.className, name, this.hwkInfo, this.file, visible, startDate, endDate).subscribe({
+        next: x => {
+          console.log(x)
+        },
+        error: err => {
+          console.log("ADDING HWK ERROR: " + err)
+        },
+        complete: () => console.log("Added Hwk")
+      });
+    } else {
+      this.testService.updateHwk(this.className, this.hwkName, name, this.hwkInfo, this.file, visible, startDate, endDate).subscribe({
+        next: x => {
+          console.log(x)
+        },
+        error: err => {
+          console.log("UPDATING HWK ERROR: " + err)
+        },
+        complete: () => console.log("Updated Hwk")
+      });
+    }
+    
+  }
 
   ngOnInit() {
-    this.getHomework();
+    console.log("newHwk: " + this.newHwk);
+    if (this.newHwk !== undefined &&
+      this.newHwk !== null &&
+      this.newHwk != true) {
+      this.getHomework();
+    }
   }
 
 }
