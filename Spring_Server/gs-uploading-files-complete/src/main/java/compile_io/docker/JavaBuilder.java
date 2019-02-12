15506c6c230869379c25e31636913f466a6c1dc4
Mike Riley <rileyma@rose-hulman.edu>
2019-02-12 17:46:44 -0500

    some tweaks

--- Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaBuilder.java
+++ Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaBuilder.java
@@ -18,7 +18,6 @@ public class JavaBuilder extends AbstractBuilder {
         List<File> professorFiles = super.getProfessorFiles();
 
         dockerfileData.append("FROM gradle:4.3-jdk-alpine\n");
-        dockerfileData.append("RUN chown root " + super.getWorkingDirectory() + "\n");
         dockerfileData.append("USER root\n");
         dockerfileData.append("WORKDIR " + super.getWorkingDirectory() + "\n");
         dockerfileData.append("EXPOSE 8000\n");
@@ -26,11 +25,11 @@ public class JavaBuilder extends AbstractBuilder {
         dockerfileData.append("RUN mkdir -p src/test/java\n");
         dockerfileData.append("COPY build.gradle build.gradle\n");
         for (int i = 0; i < super.getNumStudentFiles(); i++) {
-            dockerfileData.append("COPY /student-files/" + studentFiles.get(i).getName() + " " + studentFiles.get(i).getName() +  "\n");
+            dockerfileData.append("COPY " + studentFiles.get(i).getParent() + "/" + studentFiles.get(i).getName() + " " + studentFiles.get(i).getName() +  "\n");
             dockerfileData.append("RUN mv " + studentFiles.get(i).getName() + " " + "src/main/java/\n");
         }
         for (int i = 0; i < super.getNumProfessorFiles(); i++) {
-            dockerfileData.append("COPY /professor-files/" + professorFiles.get(i).getName() + " " + professorFiles.get(i).getName() +  "\n");
+            dockerfileData.append("COPY " + professorFiles.get(i).getParent() + "/" + professorFiles.get(i).getName() + " " + professorFiles.get(i).getName() +  "\n");
             dockerfileData.append("RUN mv " + professorFiles.get(i).getName() + " " + "src/test/java/\n");
         }
         dockerfileData.append("CMD export GRADLE_USER_HOME=\"" + super.getWorkingDirectory() + "\" && gradle test\n");
