ba567aec75cf549ef69717c1f2811d05e91becb5
Mike Riley <rileyma@rose-hulman.edu>
2019-02-12 22:23:27 -0500

    linux vm docker stuff

--- Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaBuilder.java
+++ Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaBuilder.java
@@ -20,8 +20,8 @@ public class JavaBuilder extends AbstractBuilder {
         dockerfileData.append("FROM gradle:4.3-jdk-alpine\n");
         dockerfileData.append("WORKDIR " + super.getWorkingDirectory() + "\n");
         dockerfileData.append("EXPOSE 8000\n");
-        dockerfileData.append("RUN mkdir -p src/main/java\n");
-        dockerfileData.append("RUN mkdir -p src/test/java\n");
+        dockerfileData.append("RUN mkdir -p $HOME/src/main/java\n");
+        dockerfileData.append("RUN mkdir -p $HOME/src/test/java\n");
         dockerfileData.append("COPY build.gradle build.gradle\n");
         for (int i = 0; i < super.getNumStudentFiles(); i++) {
             dockerfileData.append("COPY /student-files/" + studentFiles.get(i).getName() + " " + studentFiles.get(i).getName() +  "\n");
