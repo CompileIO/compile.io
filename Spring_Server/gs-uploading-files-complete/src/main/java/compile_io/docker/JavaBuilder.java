a15ffe97c4839e5000a1d86d0a669324a129b6f9
Mike Riley <rileyma@rose-hulman.edu>
2019-02-12 19:32:24 -0500

    trying to get vm docker to work

--- Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaBuilder.java
+++ Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaBuilder.java
@@ -10,7 +10,7 @@ public class JavaBuilder extends AbstractBuilder {
 
     public JavaBuilder(List<File> studentFiles, List<File> professorFiles) {
         super(studentFiles, professorFiles);
-    } 
+    }
 
     public String getDockerfileData() {
         StringBuilder dockerfileData = new StringBuilder();
@@ -18,18 +18,18 @@ public class JavaBuilder extends AbstractBuilder {
         List<File> professorFiles = super.getProfessorFiles();
 
         dockerfileData.append("FROM gradle:4.3-jdk-alpine\n");
-        dockerfileData.append("USER root\n");
         dockerfileData.append("WORKDIR " + super.getWorkingDirectory() + "\n");
         dockerfileData.append("EXPOSE 8000\n");
+        dockerfileData.append("USER root\n");
         dockerfileData.append("RUN mkdir -p src/main/java\n");
         dockerfileData.append("RUN mkdir -p src/test/java\n");
         dockerfileData.append("COPY build.gradle build.gradle\n");
         for (int i = 0; i < super.getNumStudentFiles(); i++) {
-            dockerfileData.append("COPY " + studentFiles.get(i).getName() + " " + studentFiles.get(i).getName() +  "\n");
+            dockerfileData.append("COPY /student-files/" + studentFiles.get(i).getName() + " " + studentFiles.get(i).getName() +  "\n");
             dockerfileData.append("RUN mv " + studentFiles.get(i).getName() + " " + "src/main/java/\n");
         }
         for (int i = 0; i < super.getNumProfessorFiles(); i++) {
-            dockerfileData.append("COPY " + professorFiles.get(i).getName() + " " + professorFiles.get(i).getName() +  "\n");
+            dockerfileData.append("COPY /professor-files/" + professorFiles.get(i).getName() + " " + professorFiles.get(i).getName() +  "\n");
             dockerfileData.append("RUN mv " + professorFiles.get(i).getName() + " " + "src/test/java/\n");
         }
         dockerfileData.append("CMD export GRADLE_USER_HOME=\"" + super.getWorkingDirectory() + "\" && gradle test\n");
