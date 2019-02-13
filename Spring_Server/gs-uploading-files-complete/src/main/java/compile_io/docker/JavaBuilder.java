b443c97e94224e3ddb0f6608d2e124c741ee46b7
Mike Riley <rileyma@rose-hulman.edu>
2019-02-12 22:38:09 -0500

    linux vm docker stuff

--- Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaBuilder.java
+++ Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaBuilder.java
@@ -32,7 +32,7 @@ public class JavaBuilder extends AbstractBuilder {
         //     dockerfileData.append("RUN mv " + professorFiles.get(i).getName() + " " + "src/test/java/\n");
         // }
         //dockerfileData.append("CMD export GRADLE_USER_HOME=\"" + super.getWorkingDirectory() + "\" && gradle test\n");
-        dockerfileData.append("CMD cd .. && ls\n");
+        dockerfileData.append("CMD cd .. && cd src && ls\n");
 
         return dockerfileData.toString();
     }
