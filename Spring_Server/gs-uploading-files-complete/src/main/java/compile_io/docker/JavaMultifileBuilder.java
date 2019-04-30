c6233d77fdc770298ad80559cf34cb529eb2be49
Mike Riley <rileyma@rose-hulman.edu>
2019-04-30 18:52:10 -0400

    experimenting with multifile docker builder, have yet to run any form of test

--- Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaMultifileBuilder.java
+++ Spring_Server/gs-uploading-files-complete/src/main/java/compile_io/docker/JavaMultifileBuilder.java
@@ -15,7 +15,7 @@ public class JavaMultifileBuilder extends AbstractBuilder {
     public String getDockerfileData() {
         StringBuilder dockerfileData = new StringBuilder();
         List<File> studentFiles = super.getStudentFiles();
-        List<File> professorFiles = super.getProfessorFiles();
+        // List<File> professorFiles = super.getProfessorFiles();
 
         if (studentFiles.size() != 1) {
             throw new InvalidFileException("Expected single file (a zipped project), but received " + studentFiles.size() + " files.");
@@ -25,14 +25,14 @@ public class JavaMultifileBuilder extends AbstractBuilder {
         dockerfileData.append("WORKDIR " + super.getWorkingDirectory() + "\n");
         dockerfileData.append("EXPOSE 8000\n");
         dockerfileData.append("CMD unzip " + studentFiles.get(0));
-        dockerfileData.append("COPY build.gradle build.gradle\n");
+        dockerfileData.append("COPY build.gradle /" + studentFiles.get(0).getName() + "/build.gradle\n");
         dockerfileData.append("COPY " + super.getCodePath() + "/" + studentFiles.get(0).getName() + " " + studentFiles.get(0).getName() +  "\n");
-        for (int i = 0; i < super.getNumProfessorFiles(); i++) {
-        	String professorFilePath = super.getCodePath().replaceFirst("student-files", "professor-files");
-            dockerfileData.append("COPY " + professorFilePath + "/" + professorFiles.get(i).getName() + " " + professorFiles.get(i).getName() +  "\n");
-        }
+        // for (int i = 0; i < super.getNumProfessorFiles(); i++) {
+        // 	String professorFilePath = super.getCodePath().replaceFirst("student-files", "professor-files");
+        //     dockerfileData.append("COPY " + professorFilePath + "/" + professorFiles.get(i).getName() + " " + professorFiles.get(i).getName() +  "\n");
+        // }
         dockerfileData.append("CMD unzip " + studentFiles.get(0));
-        dockerfileData.append("CMD export GRADLE_USER_HOME=\"" + super.getWorkingDirectory() + "\" && gradle test\n");
+        dockerfileData.append("CMD export GRADLE_USER_HOME=\"" + super.getWorkingDirectory() + "\" && cd " + studentFiles.get(0).getName() + " && gradle test\n");
 
         return dockerfileData.toString();
     }
