ecfab59b359c760cdeb73cd5fb8af12ebb59677b
Joshua Palamuttam <joshpal97@gmail.com>
2019-05-08 12:49:57 -0400

    Made a test for AssignmentControllers

--- Spring_Server/gs-uploading-files-complete/src/test/java/compile_io/controllers/AssignmentControllerTest.java
+++ Spring_Server/gs-uploading-files-complete/src/test/java/compile_io/controllers/AssignmentControllerTest.java
@@ -1,20 +1,43 @@
 package compile_io.controllers;
 
-
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import compile_io.Application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { Application.class })
+@AutoConfigureMockMvc
 public class AssignmentControllerTest {
 
-  @Test
-  public void testTest() {
-    assertEquals(true, true);
-  }
+    @Autowired
+    private MockMvc mockMvc;
 
+
+    @Test
+    public void testGetAssignments() throws Exception {
+      mockMvc.perform(MockMvcRequestBuilders.get("/Assignment").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+//      console.log()
+      
+//      this.mockMvc.perform(get("/").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+//      .andExpect(status().isOk())
+//      .andExpect(content().contentType("application/json"));
+
+    }
 }
